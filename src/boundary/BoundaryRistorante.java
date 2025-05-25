871636605f33bb1c47faf8d3d1b75ff79c5256f4
salvatoreferraro <salvatoreferraro@localhost>
2025-05-25 17:18:34 +0200

    boundary ristorante

--- src/boundary/BoundaryRistorante.java
+++ src/boundary/BoundaryRistorante.java
@@ -39,54 +39,61 @@ public class BoundaryRistorante {
 	    }
 	 
 
-	   public static void acquistaProdotto() {
-		   Scanner scan = new Scanner(System.in);
+	    public static void acquistaProdotto() {
+	        try {
+	            Scanner scan = new Scanner(System.in);
 
-		   int idPescheria = 0;
-		   int idProdotto = 0;
-		   int quantita = 0;
-		   boolean inputValido = false;
+	            int idPescheria = 0;
+	            int idProdotto = 0;
+	            int quantita = 0;
+	            boolean inputValido = false;
 
-		   while (!inputValido) {
-		       try {
-		           System.out.println("Inserisci l'ID della pescheria");
-		           idPescheria = Integer.parseInt(scan.nextLine());
+	            while (!inputValido) {
+	                try {
+	                    System.out.println("Inserisci l'ID della pescheria");
+	                    idPescheria = Integer.parseInt(scan.nextLine());
 
-		           System.out.println("Inserisci l'ID del prodotto");
-		           idProdotto = Integer.parseInt(scan.nextLine());
+	                    System.out.println("Inserisci l'ID del prodotto");
+	                    idProdotto = Integer.parseInt(scan.nextLine());
 
-		           System.out.println("Inserisci la quantità del prodotto");
-		           quantita = Integer.parseInt(scan.nextLine());
+	                    System.out.println("Inserisci la quantità del prodotto");
+	                    quantita = Integer.parseInt(scan.nextLine());
 
-		           if (idPescheria <= 0 || idProdotto <= 0 || quantita <= 0) {
-		               throw new IllegalArgumentException("I valori devono essere positivi.");
-		           }
+	                    if (idPescheria <= 0 || idProdotto <= 0 || quantita <= 0) {
+	                        throw new IllegalArgumentException("I valori devono essere positivi.");
+	                    }
 
-		           inputValido = true;
-		       } catch (IllegalArgumentException e) {
-		           System.out.println("Errore nell'acquisizione dei dati, riprovare..");
-		           System.out.println();
-		       }
-		   }
-        
-			   ArrayList<String> results = GestioneOrdini.getInstance().acquistaProdotto(idPescheria, idProdotto, quantita);
+	                    inputValido = true;
+	                } catch (IllegalArgumentException e) {
+	                    System.out.println("Errore nell'acquisizione dei dati, riprovare..");
+	                    System.out.println();
+	                }
+	            }
 
-       
-			   System.out.println("Prezzo totale: " + results.get(0) + " euro");
-			   System.out.println("Digita 'S' per confermare o qualunque altro carattere per annullare..");
-			   String conferma = scan.nextLine();
+	            ArrayList<String> results = GestioneOrdini.getInstance().acquistaProdotto(idPescheria, idProdotto, quantita);
 
-			   if (!conferma.equals("S") && !conferma.equals("s")) {
-            System.out.println("Operazione annullata..");
-            System.out.println();
-            return;
-			   }
+	            System.out.println("Prezzo totale: " + results.get(0) + " euro");
+	            System.out.println("Digita 'S' per confermare o qualunque altro carattere per annullare..");
+	            String conferma = scan.nextLine();
 
-			   System.out.println("Ordine confermato con successo!");
-			   System.out.println("Prezzo totale: " + results.get(0) + " euro");
+	            if (!conferma.equals("S") && !conferma.equals("s")) {
+	                System.out.println("Operazione annullata..");
+	                System.out.println();
+	                return;
+	            }
 
-	   }
-	   
-}
+	            System.out.println("Ordine confermato con successo!");
+	            System.out.println("Prezzo totale: " + results.get(0) + " euro");
+
+	        } catch (OperationException oE) {
+	            System.out.println(oE.getMessage());
+	            System.out.println("Riprovare..\n");
+	        } catch (Exception e) {
+	            System.out.println("Unexpected exception, riprovare..");
+	            System.out.println();
+	        }
+	    } 
+}  
+	    
 
 	   
